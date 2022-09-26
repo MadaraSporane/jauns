436c1f5e82eab29c16304e14cd8cea47828e0cf9
MadaraSporane <madara.sporane@gmail.com>
2022-09-26 21:35:56 +0300

    Initial commit

---
+++ src/main/java/lekcijaViens/labDarbs/Main.java
@@ -0,0 +1,41 @@
+package lekcijaViens.labDarbs;
+
+public class Main {
+    public static void main(String[] args) {
+        //int - integer satur veselu skaitli
+
+        int vecums = 31;
+        System.out.println("mans vecums ir:" +vecums);
+int garums = 65;
+System.out.println("mans garums ir:" + garums);
+
+String sunaVards= "Tedis";
+System.out.print("Suna vards ir" + sunaVards);
+String teksts= "Suna vārds ir:";
+//maksimālā integer vērtība
+int maxInt = 378736723;
+ byte vecumsDivi = 99;
+
+ float apavuIzmers = 44.5F;
+ System.out.println("apavu izmers"+ apavuIzmers);
+
+ boolean isOpen = true;
+ boolean isClosed = false;
+ System.out.println("Vai veikals ir atvērts?" + isOpen);
+
+ char valuta = '$';
+ System.out.println(valuta);
+
+ //var vertibu nepieskirt. japieskir velak
+        int summa;
+        summa = 5;
+        System.out.println(summa);
+
+int x = 10;
+int y = -45;
+int rezultats = x/y;
+System.out.println(rezultats);
+
+
+    }
+}
