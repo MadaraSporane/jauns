460b31d3724a66f9c7641b1f13c0428400e6da45
MadaraSporane <madara.sporane@gmail.com>
2022-09-30 14:36:05 +0300

    FirstClass.java
    majasdarbs

---
+++ src/main/java/lekcijaViens/FirstClass.java
@@ -0,0 +1,48 @@
+package lekcijaViens;
+
+public class FirstClass {
+    public static void main(String[] args) {
+
+
+        String valstsNosaukums = ("Itālija");
+                System.out.println(valstsNosaukums);
+
+                int iedzivotajuSkaits = 55;
+                System.out.println("Iziedzīvotāju skaits:" + iedzivotajuSkaits + " miljoni");
+
+        int platiba =  301230;
+        System.out.println("Platība:" + platiba + "km²");
+
+
+        String galavaspilseta = ("Roma");
+        System.out.println("Galvaspilsēta: " + galavaspilseta);
+
+        String valoda = ("Itāļu");
+                System.out.println("Valoda:"+ valoda);
+
+                boolean VaiIrEsValsts = true;
+                System.out.println("Vai ir ES dalībvalsts:"+ VaiIrEsValsts);
+
+                char valuta = '€';
+                System.out.println ("Valūta: " + valuta + "(Eur)");
+
+System.out.println("Itālijas nacionālā valoda ir " +valoda + " un vietējie izvēlas citās \n valodās nerunāt. Viens kruasāns maksā 1 " +valuta+
+         " Viens no populārākajiem galamērķiem ir "+ galavaspilseta+".");
+
+System.out.print("Aprēķinu piemēri:" );
+
+int x = 10;
+int y = 5;
+
+int rezultats= x+y;
+System.out.println(x+y);
+int rezultats1= x-y;
+System.out.println(x-y);
+int rezultats2= x/y;
+System.out.println(x/y);
+
+
+
+
+    }
+}
